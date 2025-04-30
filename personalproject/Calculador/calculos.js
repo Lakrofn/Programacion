a17e87b21667a88c94aa0bac412335ef4eb262cb
lakrofn <alvarocrucesmartinez@gmail.com>
2025-05-01 00:31:44 +0200

    1/5/2025

--- personalproject/Calculador/calculos.js
+++ personalproject/Calculador/calculos.js
@@ -1,64 +1,58 @@
-let pantalla = document.getElementById('pantalla');
 let num1 = '';
+num1 = Number(num1);
 let num2 = '';
-let operator = '';
-let resultado = '';
+num2 = Number(num2);
+let operador = '';
 
-function numeros(numero){
-    if(operator == ''){
-        num1 += numero;
-    }else{
-        num2 += numero;
+    function numeros(valor) {
+        if (operador === '') {
+            num1 += valor; // Escribe el primer número
+        } else {
+            num2 += valor; // Escribe el segundo número
+        }
+            actualizarpantalla(); // Actualiza la pantalla
     }
-pantalla.value = num1;
-}
 
-function limpiar(){
-    num1 = '';
-    num2 = '';
-    operator = '';
-    pantalla.value = '';
-}
+    function operadores(valor) {
+        if (num1 !== '') {
+            operador = valor; // Guarda el operador
+        }
+            actualizarpantalla(); // Actualiza la pantalla
+    }
 
-function sumar(){
- if(operator == '+'){
-    resultado = num1 + num2;
- }
-}
+    function limpiar() {
+        num1 = '';
+        num2 = '';
+        operador = '';
+        actualizarpantalla(); // Limpia la pantalla
+    }
 
-function restar(){
- if(operator == '-'){
-    resultado = num1 - num2;
- }
-}
+    function calculo() {
+        let resultado = '';
 
-function multiplicar(){
- if(operator == '*'){
-    resultado = num1 * num2;
- }
-}
+        if (operador === '+') {
+            resultado = num1 + num2;
+        } else if (operador === '-') {
+            resultado = num1 - num2;
+        } else if (operador === '*') {
+            resultado = num1 * num2;
+        } else if (operador === '^') {
+            resultado = Math.pow(num1, num2);
+        } else if (operador === '/') {
+            if (num2 !== '0') {
+                resultado = num1 / num2;
+            } else {
+                resultado = 'Error'; // Maneja en caso de división por cero
+            }
+        }
 
-function dividir(){
- if(operator == '/'){
-    resultado = num1 / num2;
- }
-}
+        num1 = resultado; // Guarda el resultado como el nuevo num1
+        num2 = ''; // Limpia el num2
+        operador = ''; // Limpia el operador
+        actualizarpantalla(); // Muestra el resultado
+    }
 
-function calcular(){
- if(operator == '^'){
-    resultado = Math.pow(num1, num2);
- }
-}
-
-function igualar(){
- if(operator == '='){
-    resultado = eval(num1 + operator + num2);
- }
-}
-
-function calculo(){
-    num1 = pantalla.value;
-    operator = document.querySelector('.operator').innerText;
-    igualar();
-    pantalla.value = resultado;
-}
+function actualizarpantalla() {
+        let display = document.getElementById('pantalla');
+        pantalla.value = num1 + operador + num2; // Muestra lo que se ha escrito
+    }
