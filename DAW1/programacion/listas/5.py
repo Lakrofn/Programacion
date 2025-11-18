3ede0c987b2408494d2169019e53ca374284a2df
lakrofn <alvarocrucesmartinez@gmail.com>
2025-11-18 18:36:24 +0100

    Inicio de proyecto del juego de domino e iniciación de boletín de HTML sobre tablas y formularios

--- DAW1/programacion/listas/5.py
+++ DAW1/programacion/listas/5.py
@@ -1,5 +1,96 @@
-def encajan():
-    ficha1 = [4,5]
-    ficha2 = [6,5]
-    if ficha1[0] == ficha2[0]:
-        
+import random
+
+def domino():
+    # Estas variables son para almacenar las fichas
+    fichas = [[0,0],[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[1,1],[1,2],[1,3],[1,4],[1,5],[1,6],[2,2],[2,3],[2,4],[2,5],[2,6],[3,3],[3,4],[3,5],[3,6],[4,4],[4,5],[4,6],[5,5],[5,6],[6,6]]
+    jugador1 = []
+    jugador2 = []
+    movimiento = []
+    contador = 0
+
+    # Estas variables es para comprobar las fichas que se encuentran en cada extremo
+    ultima_ficha = []
+    primera_ficha = []
+
+    # Esta variable indica la partida actual
+    jugadas =[]
+
+    # Fichas para el jugador 1
+    while contador < 7:
+        movimiento = fichas.pop(random.randint(0, len(fichas)-1))
+        jugador1.append(movimiento)
+        movimiento = []
+        contador += 1
+    contador = 0
+
+    #Fichas para el jugador 2
+    while contador < 7:
+        movimiento = fichas.pop(random.randint(0, len(fichas)-1))
+        jugador2.append(movimiento)
+        movimiento = []
+        contador += 1
+    contador = 0
+    
+    doble_mas_alta1 = []
+    doble_mas_alta2 = []
+    doble_actual = []
+
+    # comprobamos quien empieza primero
+    for i in jugador1:
+        if i[0] == i[1]:
+            doble_actual.append(i)
+            if doble_actual > doble_mas_alta1:
+                doble_mas_alta1 = doble_actual
+        doble_actual = []
+    for i in jugador2:
+        if i[0] == i[1]:
+            doble_actual.append(i)
+            if doble_actual > doble_mas_alta2:
+                doble_mas_alta2 = doble_actual
+        doble_actual = []
+
+    # Comprobamos quien gana
+    if doble_mas_alta1 > doble_mas_alta2:
+        print(f"El jugador 1 gana con {doble_mas_alta1[0]}")
+        jugadas.append(doble_mas_alta1[0])
+        jugador1.remove(doble_mas_alta1[0])
+        print(f"La jugada actual es {jugadas}")
+        ultima_ficha = doble_mas_alta1[0]
+        primera_ficha = doble_mas_alta1[0]
+
+        # comprobamos si el jugador 1 tiene una ficha en la primera o ultima posicion y sino robara hasta que tenga una en caso de que no tenga aun asi pasara
+        if len(fichas) != 0:
+            while ultima_ficha[1] not in jugador2 or ultima_ficha[2] not in jugador2 or primera_ficha[1] not in jugador2 or primera_ficha[2] not in jugador2:
+                print(f"jugador 2 roba una ficha a no tener ninguna ficha compatible")
+                movimiento = fichas.pop(random.randint(0, len(fichas)-1))
+                jugador2.append(movimiento)
+                movimiento = []
+        else:
+            print(f"Jugador 2 pasa al no tener fichas")
+        eleccion = input(f"Jugador 2 elige una de tus siguientes fichas: {jugador2}")
+    elif doble_mas_alta2 > doble_mas_alta1:
+        print(f"El jugador 2 gana con {doble_mas_alta2[0]}")
+        jugadas.append(doble_mas_alta2[0])
+        jugador2.remove(doble_mas_alta2[0])
+        print(f"La jugada actual es {jugadas}")
+        ultima_ficha = doble_mas_alta2[0]
+        primera_ficha = doble_mas_alta2[0]
+
+        # comprobamos si el jugador 1 tiene una ficha en la primera o ultima posicion y sino robara hasta que tenga una en caso de que no tenga aun asi pasara
+        if len(fichas) != 0:
+            while ultima_ficha[0] not in jugador1 or ultima_ficha[1] not in jugador1 or primera_ficha[0] not in jugador1 or primera_ficha[1] not in jugador1:
+                print(f"jugador 1 roba una ficha a no tener ninguna ficha compatible")
+                movimiento = fichas.pop(random.randint(0, len(fichas)-1))
+                jugador1.append(movimiento)
+                movimiento = []
+        input(f"Jugador 1 elige una de tus siguientes fichas: {jugador1}")
+
+
+
+    # acumulable = ""
+    # for i in range(len(doble_mas_alta2)):
+    #     acumulable += f"[{doble_mas_alta2[i][0]} : {doble_mas_alta2[i][1]}] "
+    # print(f"El acumulable de la jugada es {acumulable}")
+
+
+domino()
--- DAW1/programacion/matrices/suma_matriz.py
+++ DAW1/programacion/matrices/suma_matriz.py
@@ -1,16 +1,25 @@
-matriz1 = [[5,0,0], [0,9,0],[0,0,7]]
-matriz2 = [[1,2,3], [1,0,3], [1,3,2]]
+def suma_matriz(matriz1, matriz2):
+    resultado = []
+    acumulador = []
+    for fila in range(0,len(matriz1)):
+        for celdas in range(0,len(matriz1[fila])):
+            acumulador.append(matriz1[fila][celdas] + matriz2[fila][celdas])
+        resultado.append(acumulador)
+        acumulador = []
+    return resultado
 
-fila1= []
-fila2= []
-
-acumulador = 0
-cadena=""
+print(f"La suma de las matrices es {suma_matriz([[5,0,0], [0,9,0],[0,0,7]], [[1,2,3], [1,0,3], [1,3,2]])}")
 
 
-for fila in range(1,len(matriz1)):
-    for celdas in range(fila):
-        acumulador += matriz1[fila][celdas] + matriz2[fila][celdas]
-        cadena += " " + str(acumulador)
-    print(cadena)
+def cuadrado_matriz(matriz):
+    resultado = ""
     cadena = ""
+    for fila in matriz:
+        for celdas in fila:
+            cadena += str(celdas) + " "
+        resultado += cadena + "\n"
+        cadena = ""
+    return resultado
+
+print(cuadrado_matriz([[6, 2, 3], [1, 9, 3],[1, 2, 9]]))
+
