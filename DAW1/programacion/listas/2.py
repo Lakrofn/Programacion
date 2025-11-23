35c7370db72d7626b847e604847a19764086ad83
lakrofn <alvarocrucesmartinez@gmail.com>
2025-11-23 11:15:56 +0100

    Boletin de listas terminado.

--- DAW1/programacion/listas/2.py
+++ DAW1/programacion/listas/2.py
@@ -1,6 +1,18 @@
-def desplazamiento(posicion):
+def desplazamiento(desplazar,direccion):
     lista = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-    lista= str(lista[-posicion]) + str(lista[:-posicion])
-    return lista
 
-print(desplazamiento(1))
+    if direccion == "D":
+        contador = 0
+        while contador < desplazar:
+            lista.insert(0, lista.pop(-1))
+            contador += 1
+    elif direccion == "I":
+        contador = 0
+        while contador < desplazar:
+            lista.append(lista.pop(0))
+            contador += 1
+    else:
+        print("La direccion no es valida")
+    return lista
+print(desplazamiento(5,"D"))
+print(desplazamiento(5,"I"))
--- DAW1/programacion/listas/9.py
+++ DAW1/programacion/listas/9.py
@@ -1,12 +1,27 @@
 def cadena_mas_larga(lista):
     mayor = 0
+    mas_caracteres = 0
     cadena_mayor = ""
+    desempate = ""
     for i in lista:
         if len(i) > mayor:
             mayor = len(i)
             cadena_mayor = i
+            for x in i:
+                if x not in desempate:
+                    desempate += x
+                    mas_caracteres = len(desempate)
+            desempate = ""
         elif len(i) == mayor:
-            cadena_mayor +=" " + "y" + " " + i
-    return print(f"La cadena mas larga es/son {cadena_mayor} con {mayor} caracteres")
+            for x in i:
+                if x not in desempate:
+                    desempate += x
+                    if mas_caracteres < len(desempate):
+                        mas_caracteres = len(desempate)
+                        cadena_mayor = i
+            desempate = ""
 
-cadena_mas_larga(["hola", "adios", "buenos", "dias", "alvaro"])
+            
+    return print(f"La cadena mas larga es {cadena_mayor} con {mayor} caracteres de los cuales {mas_caracteres} son diferentes")
+
+cadena_mas_larga(["hola", "adios","buenos", "dias", "alvaro"])
