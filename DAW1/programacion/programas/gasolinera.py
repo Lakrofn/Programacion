ffa219c9ec52da1405c3f0634256c937d581aa03
lakrofn <alvarocrucesmartinez@gmail.com>
2025-11-26 12:27:09 +0100

    Primera pagina web empezada.

--- DAW1/programacion/programas/gasolinera.py
+++ DAW1/programacion/programas/gasolinera.py
@@ -2,7 +2,7 @@ import re
 
 
 def menu():
-    lista = ["Marca uno de los siguientes numeros: ","1. Llenas tanque (diesel o gasolina)", "2. Asignar coche al surtidor y repostar", "3. Consultar puntos de cliente", "4. Comprobar historicos de ventas", 
+    lista = ["Marca uno de los siguientes numeros: ","1. Llenas tanque (diesel o gasolina)", "2. Asignar coche al surtidor y repostar", "3. Consultar puntos de clientes", "4. Comprobar historicos de ventas", 
             "5. Consultar el estado de los surtidores", "6. Asignar precio a la gasolina/diesel", "7. Cerrar el programa y salir"]
     for i in lista:
         print(i)
@@ -26,36 +26,70 @@ def opcion1(surtidores):
                 surtidores[i] = 5000
             print(f"El sustidor {surtidor} esta con {surtidores[i]} litros")
 
-def opcion2(gasolina, diesel, surtidores):
-    acumulable
-    gasolina=[]
-    diesel=[]
+def opcion2():
+    nuevo = True
+    acumulador = []
+    clientes = [['5555KKK', 'gasolina', 10], ['6666LLL', 'diesel', 20]]
     surtidores = [5000, 5000, 5000, 5000]
+    combustible_distribuido = [0, 0 ,0, 0]
     matricula = input("Introduce la matricula de tu coche: ")
     while not re.match(r'^[1-9]{4}[A-Z]{3}$', matricula):
         matricula = input("Introduce una matricula valida: ")
 
-    if matricula not in gasolina and matricula not in diesel:
+    for i in clientes:
+        if matricula in i:
+            nuevo = False
+    if nuevo == True:
         combustible = input("Introduce el tipo de combustible (gasolina o diesel): ")
         while combustible != "gasolina" and combustible != "diesel":
             combustible = input("Introduce un tipo de combustible valido (gasolina o diesel): ")
-
-        gasolina.append(matricula) if combustible == "gasolina" else diesel.append(matricula)
+        pago = float(input("Introduce cuanto vas a pagar: "))
+        while pago < 10 or not re.match(r'^\d+(\.\d{1,2})?$', str(pago)):
+            pago = float(input("Introduce un pago valido con 2 decimales: "))
+        acumulador.append(matricula)
+        acumulador.append(combustible)
+        acumulador.append(pago)
+        clientes.append(acumulador)
     else:
         pago = float(input("Introduce cuanto vas a pagar: "))
         while pago < 10 or not re.match(r'^\d+(\.\d{1,2})?$', str(pago)):
             pago = float(input("Introduce un pago valido con 2 decimales: "))
+        for i in clientes:
+            if i[0] == matricula:
+                for j in i:
+                    if j.isdigit():
+                        clientes[i][j] += pago
         
-
     
+    for i in clientes:
+        if i[0] == matricula:
+            for j in i:
+                if j[1] == "gasolina":
+                    if combustible_distribuido[0] < combustible_distribuido[1]:
+                        print(f"Pase por el distribuidor 1")
+                        combustible_distribuido[0] += pago
+                        surtidores[0] -= pago
+                    else:
+                        print(f"Pase por el distribuidor 2")
+                        combustible_distribuido[1] += pago
+                        surtidores[1] -= pago
+                else:
+                    if combustible_distribuido[2] < combustible_distribuido[3]:
+                        print(f"Pase por el distribuidor 3")
+                        combustible_distribuido[2] += pago
+                        surtidores[2] -= pago
+                    else:
+                        print(f"Pase por el distribuidor 4")
+                        combustible_distribuido[3] += pago
+                        surtidores[3] -= pago
 
-
+opcion2()
 
 def gasolinera():
     importe = 0
-    gasolina = []
-    diesel = []
+    clientes = []
     surtidores = [5000, 5000, 5000, 4990]
+    combustible_distribuido = [0, 0, 0, 0]
 
     menu()
     opcion = int(input("Marca un numero: "))
@@ -66,7 +100,7 @@ def gasolinera():
             opcion1(surtidores)
 
         elif opcion == 2:
-            opcion2(gasolina, diesel, surtidores)
+            opcion2(clientes, surtidores, combustible_distribuido)
         
 
         menu()
@@ -75,4 +109,4 @@ def gasolinera():
 
 
 
-gasolinera()
+#gasolinera()
